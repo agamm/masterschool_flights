1718591b1775f2b8adcd7f61077df6b57b205ac1
Andy Schunke <schunke.andy@gmail.com>
2022-10-03 19:15:05 +0200

    added airline picture

--- team_klm.js
+++ team_klm.js
@@ -8,19 +8,24 @@ const url = "https://flyapi.onrender.com";
   const data_tlvber = await tlvber.json();
   const data_arnlhr = await arnlhr.json();
 
-  console.log(data_lisnyc);
+  //const respone = await fetch("https://logo.clearbit.com/klm.com");
+
+  const img = document.createElement("img");
+  img.src = "https://logo.clearbit.com/klm.com";
+  img.width = "100";
 
   const klm = document.querySelector("#klm");
   const firstP = document.createElement("p");
   const secondP = document.createElement("p");
   const thirdP = document.createElement("p");
   const h1 = document.createElement("h1");
-  h1.textContent = "KLM Airline";
+  h1.textContent = "KLM Airline:";
 
   firstP.textContent = `Lisbon to Newyork costs ${data_lisnyc.price} dollars`;
   secondP.textContent = `TelAviv to Berlin costs ${data_tlvber.price} dollars`;
   thirdP.textContent = `Stockholm to London costs ${data_arnlhr.price} dollars`;
 
+  klm.append(img);
   klm.append(h1);
   klm.append(firstP);
   klm.append(secondP);
