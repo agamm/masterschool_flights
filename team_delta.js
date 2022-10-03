2b0399aae6b0aa886e3121f25a7aec5ccbacc3d2
Elizabeth-Cz <87705292+Elizabeth-Cz@users.noreply.github.com>
2022-10-03 19:00:35 +0200

    Update team_delta.js

--- team_delta.js
+++ team_delta.js
@@ -1,1 +1,38 @@
 // Fetch all routes for the airline and insert into #delta
+const getFlightLisNyc = async () => {
+  const response = await fetch("https://flyapi.onrender.com/lis_nyc/delta");
+  const dataJson = await response.json();
+  console.log(dataJson);
+  const deltaEl = document.querySelector("#delta");
+  const urlEl = document.createElement("p");
+  const priceEl = document.createElement("p");
+  urlEl.innerText = dataJson.url;
+  priceEl.innerText = dataJson.price;
+  deltaEl.append(urlEl, priceEl);
+};
+const getFlightTlvBer = async () => {
+  const response = await fetch("https://flyapi.onrender.com/tlv_ber/delta");
+  const dataJson = await response.json();
+  console.log(dataJson);
+  const deltaEl = document.querySelector("#delta");
+  const urlEl = document.createElement("p");
+  const priceEl = document.createElement("p");
+  urlEl.innerText = dataJson.url;
+  priceEl.innerText = dataJson.price;
+  deltaEl.append(urlEl, priceEl);
+};
+const getFlightArnLhr = async () => {
+  const response = await fetch("https://flyapi.onrender.com/arn_lhr/delta");
+  const dataJson = await response.json();
+  console.log(dataJson);
+  const deltaEl = document.querySelector("#delta");
+  const urlEl = document.createElement("p");
+  const priceEl = document.createElement("p");
+  urlEl.innerText = dataJson.url;
+  priceEl.innerText = dataJson.price;
+  deltaEl.append(urlEl, priceEl);
+};
+
+getFlightLisNyc();
+getFlightTlvBer();
+getFlightArnLhr();
