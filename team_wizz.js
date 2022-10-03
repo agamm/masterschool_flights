1021dce07deb1c20b3c46e1ea119edaffbfbabe2
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2022-10-03 18:15:31 +0100

    Add code

--- team_wizz.js
+++ team_wizz.js
@@ -1,1 +1,16 @@
 // Fetch all routes for the airline and insert into #wizz
+// Fetch all routes for the airline and insert into #wizz
+
+(async () => {
+  const firstFlight = await fetch("https://flyapi.onrender.com/tlv_ber/wizz");
+  // const secondFlight = await fetch("https://flyapi.onrender.com/arn_lhr/wizz");
+  // const thirdFlight = await fetch("https://flyapi.onrender.com/lis_nyc/wizz");
+  const data = await firstFlight.json();
+  const wizz = document.getElementById("wizz");
+  const wizzBody = document.createElement("p");
+  //   const price = wizzBody.price;
+  wizzBody.innerHTML = "Wizz: " + data.price;
+  wizz.append(wizzBody);
+  //   wizz.append(price);
+  //   console.log(price);
+})();
