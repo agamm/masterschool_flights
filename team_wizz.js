e6cf5a08d3a84b131be9c9b670f41acdd575afd4
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2022-10-04 15:41:58 +0100

    updating team wizz flight prices

--- team_wizz.js
+++ team_wizz.js
@@ -1,16 +1,24 @@
 // Fetch all routes for the airline and insert into #wizz
-// Fetch all routes for the airline and insert into #wizz
 
 (async () => {
   const firstFlight = await fetch("https://flyapi.onrender.com/tlv_ber/wizz");
-  // const secondFlight = await fetch("https://flyapi.onrender.com/arn_lhr/wizz");
-  // const thirdFlight = await fetch("https://flyapi.onrender.com/lis_nyc/wizz");
-  const data = await firstFlight.json();
-  const wizz = document.getElementById("wizz");
-  const wizzBody = document.createElement("p");
-  //   const price = wizzBody.price;
-  wizzBody.innerHTML = "Wizz: " + data.price;
-  wizz.append(wizzBody);
-  //   wizz.append(price);
-  //   console.log(price);
+  const secondFlight = await fetch("https://flyapi.onrender.com/arn_lhr/wizz");
+  const thirdFlight = await fetch("https://flyapi.onrender.com/lis_nyc/wizz");
+
+  const tlvBer = await firstFlight.json();
+  const arnLhr = await secondFlight.json();
+  const lisNyc = await thirdFlight.json();
+
+  const wizzContainer = document.getElementById("wizz");
+  wizzHeader.textContent = "Wizz";
+
+  const wizzHeader = document.createElement("h2");
+  const pricesParagraph = document.createElement("p");
+
+  pricesParagraph.innerHTML = `Telaviv to Berlin price is $${tlvBer.price} <br>`;
+  pricesParagraph.innerHTML += `Stockholm to London price is $${arnLhr.price} <br>`;
+  pricesParagraph.innerHTML += `Lisburn to  New York price is $${lisNyc.price} <br>`;
+
+  wizzContainer.append(wizzHeader);
+  wizzContainer.append(pricesParagraph);
 })();
