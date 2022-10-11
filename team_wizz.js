994dcb7b7a465fc19d92b48532f9dc0315e66c5f
Joseph Ugiagbe <blessedjoe2000@yahoo.com>
2022-10-11 14:59:14 +0100

    added getRoutePrice function

--- team_wizz.js
+++ team_wizz.js
@@ -1,24 +1,19 @@
 // Fetch all routes for the airline and insert into #wizz
 
 (async () => {
-  const firstFlight = await fetch("https://flyapi.onrender.com/tlv_ber/wizz");
-  const secondFlight = await fetch("https://flyapi.onrender.com/arn_lhr/wizz");
-  const thirdFlight = await fetch("https://flyapi.onrender.com/lis_nyc/wizz");
 
-  const tlvBer = await firstFlight.json();
-  const arnLhr = await secondFlight.json();
-  const lisNyc = await thirdFlight.json();
+  //getRoutePrice function
+  const getRoutePrice = async(url) =>{
+    const response  = await fetch(url);
+    const flightData = await response.json()
+    return flightData.price;
+  }
+
+  const tlvBer = await getRoutePrice("https://flyapi.onrender.com/tlv_ber/wizz");
+  const arnLhr = await getRoutePrice("https://flyapi.onrender.com/arn_lhr/wizz");
+  const lisNyc = await getRoutePrice("https://flyapi.onrender.com/lis_nyc/wizz");
 
   const wizzContainer = document.getElementById("wizz");
-
-  const wizzHeader = document.createElement("h2");
-  wizzHeader.textContent = "Wizz";
-
-  const pricesParagraph = document.createElement("p");
-
-  pricesParagraph.innerHTML = `Telaviv to Berlin price is $${tlvBer.price} <br>`;
-  pricesParagraph.innerHTML += `Stockholm to London price is $${arnLhr.price} <br>`;
-  pricesParagraph.innerHTML += `Lisburn to  New York price is $${lisNyc.price} <br>`;
 
   // logo img
   const logoImg = document.createElement("img");
@@ -26,8 +21,15 @@
   logoImg.setAttribute("alt", "Wizz Logo");
   logoImg.style.width = "100px";
   logoImg.style.height = "100px";
-
   wizzContainer.append(logoImg);
+
+  const wizzHeader = document.createElement("h2");
+  wizzHeader.textContent = "Wizz";
   wizzContainer.append(wizzHeader);
+
+  const pricesParagraph = document.createElement("p");
+  pricesParagraph.innerHTML = `Telaviv to Berlin price is $${tlvBer} <br>`;
+  pricesParagraph.innerHTML += `Stockholm to London price is $${arnLhr} <br>`;
+  pricesParagraph.innerHTML += `Lisburn to  New York price is $${lisNyc} <br>`;
   wizzContainer.append(pricesParagraph);
 })();
