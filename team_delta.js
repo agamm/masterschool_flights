254a063520ce1a104a7132e61a9d21766e00993f
Noam Rivlin <noam.rivlin15@gmail.com>
2022-10-14 18:58:14 +0300

    changed flights var name

--- team_delta.js
+++ team_delta.js
@@ -1,20 +1,20 @@
 // not sure what kind of naming convention will be better
 
 const BASE_URL = `https://flyapi.onrender.com/`;
-const airline = "delta";
-const filghts = [
+const airline = 'delta';
+const airportCodes = [
   {
-    origin: "LIS",
-    destination: "NYC"
+    origin: 'LIS',
+    destination: 'NYC',
   },
   {
-    origin: "TLV",
-    destination: "BER"
+    origin: 'TLV',
+    destination: 'BER',
   },
   {
-    origin: "ARN",
-    destination: "LHR"
-  }
+    origin: 'ARN',
+    destination: 'LHR',
+  },
 ];
 
 const endPoint = (flight) => {
@@ -37,23 +37,23 @@ const getFilghtsJson = async () => {
 };
 
 const createDOM = (data) => {
-  const searchEl = document.createElement("div");
-  const logoEl = document.createElement("img");
-  const deltaEl = document.querySelector("#delta");
-  const routeEl = document.createElement("p");
-  const priceEl = document.createElement("p");
-  logoEl.setAttribute("src", "https://logo.clearbit.com/delta.com");
+  const searchEl = document.createElement('div');
+  const logoEl = document.createElement('img');
+  const deltaEl = document.querySelector('#delta');
+  const routeEl = document.createElement('p');
+  const priceEl = document.createElement('p');
+  logoEl.setAttribute('src', 'https://logo.clearbit.com/delta.com');
   const { price, origin, destination } = data;
   priceEl.innerText = `$${price}`;
   routeEl.textContent = `${origin} `;
   routeEl.textContent += `${destination}`;
 
   // layout and styling
-  searchEl.style.display = "flex";
-  searchEl.style.gap = "1rem";
-  logoEl.style.width = "40px";
-  logoEl.style.height = "40px";
-  priceEl.style.fontWeight = "600";
+  searchEl.style.display = 'flex';
+  searchEl.style.gap = '1rem';
+  logoEl.style.width = '40px';
+  logoEl.style.height = '40px';
+  priceEl.style.fontWeight = '600';
   searchEl.append(logoEl, routeEl, priceEl);
   deltaEl.appendChild(searchEl);
 };
