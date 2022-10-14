fb2c24b22c04940f5919cf5925e06b998ef00d37
Elizabeth-Cz <87705292+Elizabeth-Cz@users.noreply.github.com>
2022-10-14 18:34:44 +0200

    Liz noam (#2)
    * Update team_delta.js
    
    * Update team_delta.js
    
    * Update team_delta.js
    
    * added logo and styling, refactored code
    
    * refactored code, fixed issues
    
    * separated DOM creation from fetching data
    
    * changed flights var name
    
    Co-authored-by: Noam Rivlin <noam.rivlin15@gmail.com>

--- team_delta.js
+++ team_delta.js
@@ -1,1 +1,68 @@
-// Fetch all routes for the airline and insert into #delta
+// not sure what kind of naming convention will be better
+
+const BASE_URL = `https://flyapi.onrender.com/`;
+const airline = 'delta';
+const airportCodes = [
+  {
+    origin: 'LIS',
+    destination: 'NYC',
+  },
+  {
+    origin: 'TLV',
+    destination: 'BER',
+  },
+  {
+    origin: 'ARN',
+    destination: 'LHR',
+  },
+];
+
+const endPoint = (flight) => {
+  return `${BASE_URL}${flight.origin.toLowerCase()}_${flight.destination.toLowerCase()}/${airline}`;
+};
+
+const getFilghtsJson = async () => {
+  const responses = await Promise.all(
+    filghts
+      .filter((flight) => endPoint(flight))
+      .map(async (flight) => {
+        const response = await fetch(endPoint(flight));
+        const dataJson = await response.json();
+        dataJson.origin = flight.origin;
+        dataJson.destination = flight.destination;
+        return dataJson;
+      })
+  );
+  return responses;
+};
+
+const createDOM = (data) => {
+  const searchEl = document.createElement('div');
+  const logoEl = document.createElement('img');
+  const deltaEl = document.querySelector('#delta');
+  const routeEl = document.createElement('p');
+  const priceEl = document.createElement('p');
+  logoEl.setAttribute('src', 'https://logo.clearbit.com/delta.com');
+  const { price, origin, destination } = data;
+  priceEl.innerText = `$${price}`;
+  routeEl.textContent = `${origin} `;
+  routeEl.textContent += `${destination}`;
+
+  // layout and styling
+  searchEl.style.display = 'flex';
+  searchEl.style.gap = '1rem';
+  logoEl.style.width = '40px';
+  logoEl.style.height = '40px';
+  priceEl.style.fontWeight = '600';
+  searchEl.append(logoEl, routeEl, priceEl);
+  deltaEl.appendChild(searchEl);
+};
+
+const getFlightDataAndCreateDom = async (flightJsons) => {
+  flightJsons = await getFilghtsJson();
+  flightJsons.forEach((flightJson) => {
+    createDOM(flightJson);
+  });
+};
+
+getFlightDataAndCreateDom();
